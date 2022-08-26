89b5f845b4077d16c67856fdf87b71091a7fca54
Juan Carlo <46091996+charsplusjk@users.noreply.github.com>
2022-08-26 12:07:19 -0300

    change: refactoring user

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-	import SuperiorBar from "./components/SuperiorBar.svelte";
+	
 	import Title from "./components/Title.svelte";
+	import User from "./components/User.svelte";
 	import type IUser from "./interfaces/IUser";
 
-	let user: IUser = {
-		login: 'cu',
-		name: 'string',
-		avatar_url: 'string',
-		profile_url: 'string',
-		public_repos: 0,
-		followers: 0
-	};
+	let user: IUser | null = null;
 
 	let inputValue = '';
 
 	function formSubmit() {
-		console.log(inputValue);
+		user = {
+			login: 'cu',
+			name: 'string',
+			avatar_url: 'string',
+			profile_url: 'string',
+			public_repos: 0,
+			followers: 0
+		}
 	}
 </script>
 
@@ -33,35 +34,9 @@
 		</div>
 	</header>
 
-	<div class="user-card">
-		<SuperiorBar />
-
-		<div class="user">
-			<div class="container-photo">
-				<a href={user.profile_url} target="_blank" rel="noopener">
-					<div 
-						class="user-photo"
-						style:background-image= "url({user.avatar_url})"	
-					></div>
-				</a>
-			</div>
-
-			<div class="user-details">
-				<div class="info">
-					Name: <span>{user.name}</span>
-				</div>
-				<div class="info">
-					User: <span>{user.login}</span>
-				</div>
-				<div class="info">
-					Followers: <span>{user.followers}</span>
-				</div>
-				<div class="info">
-					Repos: <span>{user.public_repos}</span>
-				</div>
-			</div>
-		</div>
-	</div>
+	{#if user}
+		<User user={user} />
+	{/if}
 </div>
 
 <style>
@@ -129,45 +104,5 @@
 		background: #4590ff;
 	}
 
-	.user-card {
-		margin-top: 65px;
-	}
-
-	.user {
-		padding: 28px 0;
-		background: rgba(255, 255, 255, 0.5);
-		box-shadow: -12px 37px 45px rgba(133, 127, 201, 0.18);
-		border-radius: 0px 0px 13px 13px;
-
-		display: flex;
-		justify-content: center;
-	}
-
-	.container-photo {
-		margin-right: 81px;
-	}
-
-	.user-photo {
-		width: 12.75rem;
-		height: 12.75rem;
-		border: 4.56px solid #2e80fa;
-		border-radius: 50%;
-		background-size: cover;
-	}
-
-	.user-details {
-		margin-right: 55px;
-	}
-
-	.user-details > .info {
-		font-weight: 600;
-		font-size: 20px;
-		line-height: 31px;
-		color: #395278;
-	}
-
-	.user-details > .info > span {
-		color: #6781a8;
-		font-weight: normal;
-	}
+	
 </style>
---
+++ src/components/User.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+    import type IUser from "../interfaces/IUser";
+
+    import SuperiorBar from "./SuperiorBar.svelte";
+
+    export let user: IUser;
+</script>
+
+<div class="user-card">
+    <SuperiorBar />
+
+    <div class="user">
+        <div class="container-photo">
+            <a href={user.profile_url} target="_blank" rel="noopener">
+                <div 
+                    class="user-photo"
+                    style:background-image= "url({user.avatar_url})"	
+                ></div>
+            </a>
+        </div>
+
+        <div class="user-details">
+            <div class="info">
+                Name: <span>{user.name}</span>
+            </div>
+            <div class="info">
+                User: <span>{user.login}</span>
+            </div>
+            <div class="info">
+                Followers: <span>{user.followers}</span>
+            </div>
+            <div class="info">
+                Repos: <span>{user.public_repos}</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .user-card {
+		margin-top: 65px;
+	}
+
+	.user {
+		padding: 28px 0;
+		background: rgba(255, 255, 255, 0.5);
+		box-shadow: -12px 37px 45px rgba(133, 127, 201, 0.18);
+		border-radius: 0px 0px 13px 13px;
+
+		display: flex;
+		justify-content: center;
+	}
+
+	.container-photo {
+		margin-right: 81px;
+	}
+
+	.user-photo {
+		width: 12.75rem;
+		height: 12.75rem;
+		border: 4.56px solid #2e80fa;
+		border-radius: 50%;
+		background-size: cover;
+	}
+
+	.user-details {
+		margin-right: 55px;
+	}
+
+	.user-details > .info {
+		font-weight: 600;
+		font-size: 20px;
+		line-height: 31px;
+		color: #395278;
+	}
+
+	.user-details > .info > span {
+		color: #6781a8;
+		font-weight: normal;
+	}
+</style>
